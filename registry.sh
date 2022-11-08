fbce3e652853f52609b26cb017a93eef63a69149
Victor Lee <victorlee079@gmail.com>
2022-11-08 15:34:16 +0800

    Update registry.sh

--- registry.sh
+++ registry.sh
@@ -1,3 +1,4 @@
+# Deploying a private registries
 docker run -d -p 5000:5000 --restart=always --name registry registry:2
 docker tag search:1.0 localhost:5000/search:1.0
 docker push localhost:5000/search:1.0
